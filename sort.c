abd6a74e536ee90209aac626b907d22506db7861
JamisonBall <jaball1993@aol.com>
2021-12-07 12:49:42 -0800

    sort now uses parallel quicksort

--- sort.c
+++ sort.c
@@ -1,8 +1,17 @@
-void par_odd_even_sort(int*, int);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+//function headers
+void par_quicksort(int*, int, int);
+int partition(int*, int, int);
 
 int main(int argc, char **argv) {
 	int *data = (int*)malloc(sizeof(int) * 25); //create array to hold data
 	time_t seed; //for generating random numbers
+	double omp_sort_start;
+	double omp_sort_finish;
 	
 	//iterators
 	int i;
@@ -11,7 +20,7 @@ int main(int argc, char **argv) {
 	
 	srand(time(&seed));
 	
-	//use threads to quickly generate numbers to fill array
+	//use threads to quickly generate random numbers to fill array
 	#pragma omp parallel for default(none), shared(data,i) num_threads(5)
 	for(i = 0; i < 25; i++) {
 		data[i] = rand() % 25;
@@ -24,41 +33,52 @@ int main(int argc, char **argv) {
 		printf("%d ", data[j]);
 	}
 	printf("\n");
-	
-	par_odd_even_sort(data, 25);
+	omp_sort_start = omp_get_wtime();
+	par_quicksort(data, 0, 25);
+	omp_sort_finish = omp_get_wtime();
 	printf("After sort:\n");
 	
 	for(k = 0; k < 25; k++) {
 		printf("%d ", data[k]);
 	}
 	printf("\n");
+	
+	printf("Parallel sort finished in %f seconds.\n",
+			omp_sort_finish - omp_sort_start);
+	free(data);
 	return 0;
 }
 
-void par_odd_even_sort(int *data, int size) {
-	int i;
-	int even_odd;
-	int temp;
+void par_quicksort(int *data, int p, int r) {
+	if(p < r) {
 	
-	for(even_odd = 0; even_odd < size; even_odd++) {
-		if(even_odd % 2 == 0) {
-			#pragma omp parallel for default(none) shared(data,size) private(i,temp) num_threads(5)
-			for(i = 1; i < size - 1; i += 2) {
-				if(data[i- 1] > data[i]) {
-					temp = data[i];
-					data[i] = data[i - 1];
-					data[i - 1] = temp;
-				}
-			}
-		} else {
-			#pragma omp parallel for default(none) shared(data,size) private(i,temp) num_threads(5)
-			for(i = 1; i < size; i += 2) {
-				if(data[i] > data[i + 1]) {
-					temp = data[i];
-					data[i] = data[i + 1];
-					data[i + 1] = temp;
-				}
-			}
+		int q = partition(data, p, r);
+		
+		//I chose to use 2 threads here to limit idle threads
+		#pragma omp parallel firstprivate(data, p, q) num_threads(2)
+		{
+			par_quicksort(data, p, q - 1);
+			par_quicksort(data, q + 1, r);
 		}
 	}
+}
+
+int partition(int *data, int p, int r) {
+	int x = data[r];
+	int i = p - 1;
+	int j;
+	int temp;
+	
+	for(j = p; j < r; j++) {
+		if(data[j] <= x) {
+			i = i + 1;
+			temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
+		}
+	}
+	temp = data[i + 1];
+	data[i + 1] = data[r];
+	data[r] = temp;
+	return i + 1;
 }
