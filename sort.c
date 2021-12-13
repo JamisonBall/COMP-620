a83127eba93b1e7ca844648bde011e1a6cf5a02f
JamisonBall <jaball1993@aol.com>
2021-12-12 19:27:06 -0800

    Minor bug fix

--- sort.c
+++ sort.c
@@ -13,28 +13,24 @@ int main(int argc, char **argv) {
 	double omp_sort_start;
 	double omp_sort_finish;
 	
-	//iterators
 	int i;
 	int j;
 	int k;
 	
 	srand(time(&seed));
-	
-	//use threads to quickly generate random numbers to fill array
-	#pragma omp parallel for default(none), shared(data,i) num_threads(5)
+	#pragma omp parallel for shared(data, i) num_threads(5)
 	for(i = 0; i < 25; i++) {
 		data[i] = rand() % 25;
 	}
 	
 	printf("Before sort:\n");
-	
-	#pragma omp parallel for default(none), shared(data,j) num_threads(5)
+	#pragma omp parallel for shared(data, j) num_threads(5)
 	for(j = 0; j < 25; j++) {
 		printf("%d ", data[j]);
 	}
 	printf("\n");
 	omp_sort_start = omp_get_wtime();
-	par_quicksort(data, 0, 25);
+	par_quicksort(data, 0, 24);
 	omp_sort_finish = omp_get_wtime();
 	printf("After sort:\n");
 	
@@ -51,17 +47,27 @@ int main(int argc, char **argv) {
 
 void par_quicksort(int *data, int p, int r) {
 	if(p < r) {
-	
 		int q = partition(data, p, r);
 		
-		//I chose to use 2 threads here to limit idle threads
-		#pragma omp parallel firstprivate(data, p, q) num_threads(2)
+		#pragma omp parallel shared(data, p, q, r) num_threads(2)
 		{
-			par_quicksort(data, p, q - 1);
-			par_quicksort(data, q + 1, r);
+		//I chose to use 2 threads here to limit idle threads
+			#pragma omp sections
+			{
+				#pragma omp section
+				{
+					par_quicksort(data, p, q - 1);
+				}
+				#pragma omp section
+				{
+					par_quicksort(data, q + 1, r);
+				}
+			}
 		}
+		
 	}
 }
+
 
 int partition(int *data, int p, int r) {
 	int x = data[r];
@@ -71,12 +77,14 @@ int partition(int *data, int p, int r) {
 	
 	for(j = p; j < r; j++) {
 		if(data[j] <= x) {
-			i = i + 1;
+			i++;
 			temp = data[i];
 			data[i] = data[j];
 			data[j] = temp;
+			
 		}
 	}
+	
 	temp = data[i + 1];
 	data[i + 1] = data[r];
 	data[r] = temp;
